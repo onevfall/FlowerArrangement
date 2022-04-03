5793c7521244461f95c528b4d009733d05388d97
LIUC2002 <2466445001@qq.com>
2022-04-03 11:47:14 +0800

    initialize with CoreData

---
+++ FlowerArrangementv1.1/FlowerArrangementv1.1/FlowerArrangementv1_1App.swift
@@ -0,0 +1,20 @@
+//
+//  FlowerArrangementv1_1App.swift
+//  FlowerArrangementv1.1
+//
+//  Created by 刘畅 on 2022/4/3.
+//
+
+import SwiftUI
+
+@main
+struct FlowerArrangementv1_1App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
